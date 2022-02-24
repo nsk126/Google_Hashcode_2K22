3811dddf58008aa2aa4b38658de9018a78adbf1e
Suresh <nsk126@gmail.com>
2022-02-25 02:17:55 +0530

    incomplete - for another day

--- MnT.cpp
+++ MnT.cpp
@@ -4,15 +4,50 @@
 
 #define LOG(x) std::cout << x << std::endl;
 
+struct Person
+{
+    char name[20];
+    uint8_t skills;
+
+    struct Skill
+    {
+        
+    };
+    
+};
+
 int main(int argc, char const *argv[])
 {
-    int C,P;
+    uint C,P;
     std::cin >> C;
     std::cin >> P;
     
     // LOG(C);LOG(P);
 
-    std::string person;
+    for (uint i = 0; i < C; i++)
+    {
+        
+        char person[20];
+        std::cin >> person;
+
+        uint8_t skills;
+        std::cin >> skills;
+
+        for (uint j = 0; j < skills; j++)
+        {
+            char skill[20];
+            std::cin >> skill;
+
+            uint8_t level;
+            std::cin >> level;
+
+
+        }
+        
+
+    }
+    
+
 
     
     return 0;
